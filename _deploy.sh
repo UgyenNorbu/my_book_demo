5e32289573d5c47e1726d06503a58582fe89b6b6
Ugyen Norbu <unorbu2019@gmail.com>
2020-06-27 18:24:12 +0600

    commit

--- _deploy.sh
+++ _deploy.sh
@@ -5,11 +5,10 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "unorbu@rsta.gov.bt"
+git config --global user.name "Ugyen Norbu"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
+git clone -b gh-pages https://github.com/UgyenNorbu/my_book_demo.git
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
